9eea5e132e14d4ecadd35fe7f8922691d521b94c
Sanjeev Rai <srai089@gmail.com>
2023-06-21 18:46:54 +0530

    update

--- src/app/allblogs/[blog]/page.js
+++ src/app/allblogs/[blog]/page.js
@@ -1,4 +1,4 @@
-// 'use client'
+'use client'
 
 // import data from '@/data/data';
 
@@ -6,11 +6,11 @@ const Blog = ({params}) => {
   // const [blogdata]= data.filter((item)=>{
   //   return item.slug==params.blog;
   // })
-  
+  const blogname= params.blog;
   return (
     <>
     <h1>My blog</h1>
-   <p>my blog detail {params.blog}</p>
+   <p>my blog detail {blogname}</p>
     </>
   )
 }
